7528c2df4319fe81ee32c40cd9d9294db245d9c1
linaedr94 <111957614+linaedr94@users.noreply.github.com>
2026-01-17 20:24:08 +0300

    Delete learn css in 1hr directory

--- learn css in 1hr/main.js
+++
@@ -1,9 +0,0 @@
-//const mazgy = document.getElementById("buy")
-//console.log(typeof mazgy)
-//console.log(mazgy)
-//mazgy.style.color="black"
-//mazgy.style.backgroundColor="purple"
- 
- const myheder= document.getElementsByClassName("myhader")
- //console.log(myheder)
-
